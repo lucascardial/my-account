800b919d2bb015d277f54a8e317ed006ba4ca6a0
Lucas Cardial <luccascardial@gmail.com>
2023-07-03 22:42:53 -0300

    feat(domain): social provider

---
+++ MyAccount.Domain/Entities/SocialProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyAccount.Domain.Validation;
+
+namespace MyAccount.Domain.Entities
+{
+	public class SocialProvider
+	{
+		public Guid Guid { get; private set; }
+		public string Name { get; private set; }
+		public bool Enabled { get; private set; }
+
+        public SocialProvider(Guid guid, string name, bool enabled)
+		{
+			Validate(name);
+
+			Guid = guid;
+			Name = name;
+			Enabled = enabled;
+		}
+
+		private static void Validate(string name)
+		{
+			DomainValidationException.When(string.IsNullOrEmpty(name),
+				"O Nome do provedor social é obrigatório");
+		}
+	}
+}
+
---
+++ MyAccount.Domain/Repositories/ISocialProviderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyAccount.Domain.Entities;
+
+namespace MyAccount.Domain.Repositories
+{
+	public interface ISocialProviderRepository
+	{
+		public Task Create(SocialProvider socialProvider);
+		public Task Update(SocialProvider socialProvider);
+		public Task getAll(SocialProvider socialProvider);
+    }
+}
+
