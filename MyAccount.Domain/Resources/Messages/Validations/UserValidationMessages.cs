2dd73dadbbf083193b9dd73e356f14b435979193
Lucas Cardial <luccascardial@gmail.com>
2023-07-03 23:19:22 -0300

    feat(domain): user validation messages

---
+++ MyAccount.Domain/Resources/Messages/Validations/UserValidationMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyAccount.Domain.Resources.Messages.Validations
+{
+	public static class UserValidationMessages
+	{
+        public static string NameNullOrEmpty { get; } = "Um Nome de usuário é obrigatório";
+        public static string EmailNullOrEmpty { get; } = "O E-mail de usuário é obrigatório";
+        public static string PasswordLength { get; } = "A senha deve conter ao menos 8 caracteres";
+    }
+}
+
