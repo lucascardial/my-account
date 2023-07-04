fa129a77423b205fe4bf63de1723817eb6c42bde
Lucas Cardial <luccascardial@gmail.com>
2023-07-03 22:42:41 -0300

    feat(domain): social account

---
+++ MyAccount.Domain/Entities/SocialAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MyAccount.Domain.Validation;
+
+namespace MyAccount.Domain.Entities
+{
+	public class SocialAccount
+	{
+		public Guid Guid { get; private set; }
+		public Guid UserId { get; private set; }
+		public Guid SocialProviderId { get; private set; }
+		public string SocialId { get; private set; }
+		public string Token { get; private set; }
+
+        public SocialAccount(Guid guid, Guid userId, Guid socialProviderId, string socialId, string token)
+        {
+            Guid = guid;
+            UserId = userId;
+            SocialProviderId = socialProviderId;
+            SocialId = socialId;
+            Token = token;
+        }
+
+        private static void Validate(string socialId, string token)
+        {
+            DomainValidationException.ValidateAll(new List<(bool condition, string errorMessage)>
+            {
+                (condition: string.IsNullOrEmpty(socialId), "O SocialId é obrigatório"),
+                (condition: string.IsNullOrEmpty(token), "O Token é obrigatório"),
+            });
+        }
+    }
+}
+
---
+++ MyAccount.Domain/Repositories/ISocialAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyAccount.Domain.Entities;
+
+namespace MyAccount.Domain.Repositories
+{
+	public interface ISocialAccountRepository
+	{
+		public Task Create(SocialAccount socialAccount);
+		public Task Update(SocialAccount socialAccount);
+		public Task<List<SocialAccount>> FindUserSocialAccounts(User user);
+    }
+}
+
