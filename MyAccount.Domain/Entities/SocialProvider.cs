09c2568a72915590592e5c6c6eddb4d1929465dd
Lucas Cardial <luccascardial@gmail.com>
2023-07-06 21:08:47 -0300

    refactor: validation message

--- MyAccount.Domain/Entities/SocialProvider.cs
+++ MyAccount.Domain/Entities/SocialProvider.cs
@@ -1,18 +1,17 @@
-﻿using MyAccount.Domain.Validation;
+﻿using MyAccount.Domain.Resources.Messages.Validations;
+using MyAccount.Domain.Validation;
 
 namespace MyAccount.Domain.Entities
 {
-	public class SocialProvider
+	public class SocialProvider : Entity
 	{
-		public Guid Guid { get; private set; }
 		public string Name { get; private set; }
 		public bool Enabled { get; private set; }
 
-        public SocialProvider(Guid guid, string name, bool enabled)
+        public SocialProvider(string name, bool enabled): base()
 		{
 			Validate(name);
 
-			Guid = guid;
 			Name = name;
 			Enabled = enabled;
 		}
@@ -20,7 +19,7 @@ namespace MyAccount.Domain.Entities
 		private void Validate(string name)
 		{
 			DomainValidationException.When(string.IsNullOrEmpty(name),
-				"O Nome do provedor social é obrigatório");
+                SocialProviderValidationMessages.NameEmptyOrNull);
 		}
 	}
 }
---
+++ MyAccount.Domain/Resources/Messages/Validations/SocialProviderValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MyAccount.Domain.Resources.Messages.Validations
+{
+	public static class SocialProviderValidationMessages
+	{
+		public static string NameEmptyOrNull { get; } = "O Nome do provedor social é obrigatório";
+	}
+}
+
